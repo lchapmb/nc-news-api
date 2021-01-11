9165a93fbadfb0a10b01951050cf8a4d05ae6630
lchapmb <lchapmb@googlemail.com>
2021-01-11 13:03:46 +0000

    schemas created for articles and comments

---
+++ db/migrations/20210111125610_create_table_comments_table.js
@@ -0,0 +1,16 @@
+exports.up = function (knex) {
+  console.log('creating comments table...');
+  return knex.schema.createTable('comments', (commentTable) => {
+    commentTable.increments('comment_id').primary();
+    commentTable.string('author').references('users.username');
+    commentTable.integer('article_id').references('articles.article_id');
+    commentTable.string('votes').defaultTo(0);
+    commentTable.timestamp('created_at').defaultTo(knex.fn.now());
+    commentTable.string('body');
+  });
+};
+
+exports.down = function (knex) {
+  console.log('dropping comments table...');
+  return knex.schema.dropTable('comments');
+};
