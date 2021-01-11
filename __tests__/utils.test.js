1a538e98bb66f00f5958573ea2c50f2fd515e69e
lchapmb <lchapmb@googlemail.com>
2021-01-11 17:00:37 +0000

    working on timestamp manip for articles data

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -1,7 +1,92 @@
 const { formatArticle } = require('../db/utils/data-manipulation.js');
 
 describe('formatArticle', () => {
-  it('returns an empty object when passed an empty array', () => {
-    expect(formatArticle([])).toEqual({});
+  it('returns an empty array when passed an empty array', () => {
+    expect(formatArticle([])).toEqual([]);
   });
+
+  it('returns an object with timestamp formated to JS Data object', () => {
+    const input = [
+      {
+        title: 'Running a Node App',
+        topic: 'coding',
+        author: 'jessjelly',
+        body:
+          'This is part two of a series on how to get up and running with Systemd and Node.js. This part dives deeper into how to successfully run your app with systemd long-term, and how to set it up in a production environment.',
+        created_at: 1471522072389
+      }
+    ];
+    const output = [
+      {
+        title: 'Running a Node App',
+        topic: 'coding',
+        author: 'jessjelly',
+        body:
+          'This is part two of a series on how to get up and running with Systemd and Node.js. This part dives deeper into how to successfully run your app with systemd long-term, and how to set it up in a production environment.',
+        created_at: 'Thu Aug 18 2016 13:07:52 GMT+0100 (British Summer Time)'
+      }
+    ];
+    expect(formatArticle(input)).toEqual(output);
+  });
+
+  it('returns an object with timestamp formated to JS Data object', () => {
+    const input = [
+      {
+        title: 'Running a Node App',
+        topic: 'coding',
+        author: 'jessjelly',
+        body:
+          'This is part two of a series on how to get up and running with Systemd and Node.js. This part dives deeper into how to successfully run your app with systemd long-term, and how to set it up in a production environment.',
+        created_at: 1471522072389
+      },
+      {
+        title:
+          "The Rise Of Thinking Machines: How IBM's Watson Takes On The World",
+        topic: 'coding',
+        author: 'jessjelly',
+        body:
+          'Many people know Watson as the IBM-developed cognitive super computer that won the Jeopardy! gameshow in 2011. In truth, Watson is not actually a computer but a set of algorithms and APIs, and since winning TV fame (and a $1 million prize) IBM has put it to use tackling tough problems in every industry from healthcare to finance. Most recently, IBM has announced several new partnerships which aim to take things even further, and put its cognitive capabilities to use solving a whole new range of problems around the world.',
+        created_at: 1500584273256
+      },
+      {
+        title: '22 Amazing open source React projects',
+        topic: 'coding',
+        author: 'happyamy2016',
+        body:
+          'This is a collection of open source apps built with React.JS library. In this observation, we compared nearly 800 projects to pick the top 22. (React Native: 11, React: 11). To evaluate the quality, Mybridge AI considered a variety of factors to determine how useful the projects are for programmers. To give you an idea on the quality, the average number of Github stars from the 22 projects was 1,681.',
+        created_at: 1500659650346
+      }
+    ];
+    const output = [
+      {
+        title: 'Running a Node App',
+        topic: 'coding',
+        author: 'jessjelly',
+        body:
+          'This is part two of a series on how to get up and running with Systemd and Node.js. This part dives deeper into how to successfully run your app with systemd long-term, and how to set it up in a production environment.',
+        created_at: 'Thu Aug 18 2016 13:07:52 GMT+0100 (British Summer Time)'
+      },
+      {
+        title:
+          "The Rise Of Thinking Machines: How IBM's Watson Takes On The World",
+        topic: 'coding',
+        author: 'jessjelly',
+        body:
+          'Many people know Watson as the IBM-developed cognitive super computer that won the Jeopardy! gameshow in 2011. In truth, Watson is not actually a computer but a set of algorithms and APIs, and since winning TV fame (and a $1 million prize) IBM has put it to use tackling tough problems in every industry from healthcare to finance. Most recently, IBM has announced several new partnerships which aim to take things even further, and put its cognitive capabilities to use solving a whole new range of problems around the world.',
+        created_at: 'Thu Jul 20 2017 21:57:53 GMT+0100 (British Summer Time)'
+      },
+      {
+        title: '22 Amazing open source React projects',
+        topic: 'coding',
+        author: 'happyamy2016',
+        body:
+          'This is a collection of open source apps built with React.JS library. In this observation, we compared nearly 800 projects to pick the top 22. (React Native: 11, React: 11). To evaluate the quality, Mybridge AI considered a variety of factors to determine how useful the projects are for programmers. To give you an idea on the quality, the average number of Github stars from the 22 projects was 1,681.',
+        created_at: 'Fri Jul 21 2017 18:54:10 GMT+0100 (British Summer Time)'
+      }
+    ];
+    expect(formatArticle(input)).toEqual(output);
+  });
+
+  // returns multiple objects with timestamp formatted
+  // does not mutate input
 });
--- db/utils/data-manipulation.js
+++ db/utils/data-manipulation.js
@@ -1,6 +1,17 @@
 // extract any functions you are using to manipulate your data, into this file
 
-exports.formatArticle = () => {
+exports.formatArticle = (data) => {
   //do stuff
-  return {};
+  // date()
+
+  const finalArr = [];
+
+  if (data.length) {
+    data.forEach((article) => {
+      const newDate = new Date(article.created_at);
+      article.created_at = newDate;
+      finalArr.push(article);
+    });
+  }
+  return finalArr;
 };
